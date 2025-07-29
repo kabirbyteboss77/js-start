4367196c2016266554eb467caa2e16118df4f741
kabirbyteboss77 <kabirkaranwal765@gmail.com>
2025-07-29 13:19:12 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(1000)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 129.9866
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN')); 
+
+// +++++++++++++++++Math+++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); 
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
